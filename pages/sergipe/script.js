5139c7f69eaee91d297b19f3fe3956369026900e
manu <m4nu.lun4@hotmail.com>
2021-09-24 00:36:41 -0300

    terminando

--- pages/sergipe/script.js
+++ pages/sergipe/script.js
@@ -3,6 +3,17 @@ var contato = document.getElementById('contato')
 var overlay = document.getElementById('overlay')
 var formCancel = document.getElementById('formCancel')
 
+var overlayCriador = document.getElementById('overlay-criador')
+var criadores = document.getElementById('criadores')
+var closeCriadores = document.getElementById('close-criadores')
+
+closeCriadores.addEventListener('click', () => {
+    overlayCriador.style.display = 'none'
+})
+
+criadores.addEventListener('click', () => {
+    overlayCriador.style.display = 'flex'
+})
 
 contato.addEventListener('click', () => {
     overlay.style.display = 'flex'
